3c0e600fcd9b25cd413d748b427af54bfb5fad00
Aravind Reddy <aravindreddy255@gmail.com>
2016-04-16 21:46:53 +0530

    started create.cpp

---
+++ create.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	int N;
+	cin >> N;
+
+	vector<vector<double> > mat(N, vector<double> (N));
+
+	return 0;
+}
